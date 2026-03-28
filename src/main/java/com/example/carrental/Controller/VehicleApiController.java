19524497c9546ff72b61b5dfe68d6fee399297c3
Gelai22 <angelamarievasquez15@gmail.com>
2026-03-29 04:24:52 +0800

    Update VehicleApiController.java

--- src/main/java/com/example/carrental/Controller/VehicleApiController.java
+++ src/main/java/com/example/carrental/Controller/VehicleApiController.java
@@ -1,1 +1,24 @@
+package com.example.carrental.Controller;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/vehicles")
+@CrossOrigin(origins = "*") 
+public class VehicleApiController {
+
+    @GetMapping("/all")
+    public List<Map<String, Object>> getAllVehicles() {
+        List<Map<String, Object>> vehicles = new ArrayList<>();
+        
+        Map<String, Object> car1 = new HashMap<>();
+        car1.put("id", 1);
+        car1.put("brand", "Toyota");
+        car1.put("model", "Vios");
+        car1.put("price", 1500);
+        
+        vehicles.add(car1);
+        return vehicles;
+    }
+}
